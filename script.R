35c5fc8ab55152b83cf599b4bba46f9e7a1e5b87
Floriano Victor <rotc21@gmail.com>
2014-11-28 16:59:59 -0200

    init

---
+++ script.R
@@ -0,0 +1,37 @@
+library(dplyr)
+library(clusterSim)
+library(class)
+library(gmodels)
+
+petr3 <- read.csv("petr3.csv", stringsAsFactor = FALSE)
+
+data <- data.Normalization(petr3[, 2:7], type="n4", normalization="column")
+data <- tbl_df(data) %>%
+        mutate(up_10_days = FALSE) 
+
+total <- nrow(data)
+for(indice in 1:total){
+  a <- data[indice,4]
+  b <- data[indice + 10 ,4]
+  if(!is.na(b[1,1])){
+    if(a[1,1] < b[1,1]){
+      data[indice,7] = TRUE
+    }
+  }else{
+    data[indice,7] = NA
+  }
+}
+data <- data[complete.cases(data),]
+data_train <- data[1:3034,]
+data_test <- data[3035:3834,]
+
+data_train_label <- data[1:3034,7]
+data_test_label <- data[3035:3834,7]
+
+dim(data_train)
+dim(data_test)
+dim(data_train_label)
+
+pred <- knn(train = data_train, test = data_test, cl = data_train_label, k = 61)
+
+Crosstable(x = data_test_label, y = pred, prop.chisq = FALSE)
